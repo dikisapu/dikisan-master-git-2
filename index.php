9d2d15eb8d623806d0d42ecc1a39e4521dae659a
dikisanggwp <backupkamera3300@gmail.com>
2023-08-20 13:44:19 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git remote</title>
+</head>
+<body>
+    <h2>Hello world</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis fuga aliquid vitae quasi eum atque earum facere! Soluta eaque, corrupti minima doloremque voluptate repudiandae. Magnam earum corrupti vel iste ut.
+    Officia sint ut omnis quisquam eaque fugit. Possimus quidem omnis aperiam perferendis error, quae placeat. Dolores iusto minus doloremque quia unde quisquam, quae libero tempora nam quaerat? Enim, tempore totam?
+    Perferendis placeat mollitia aperiam. Id quidem vitae possimus nisi totam tempora magni quis voluptatum ad? Vero repudiandae, iste cupiditate consectetur earum dolorem amet. Fuga at suscipit facilis, adipisci in est?</p>
+</body>
+</html>
