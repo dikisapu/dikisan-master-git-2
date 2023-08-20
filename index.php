1991d785c61da1703c35224676577ed0479de9a2
dikisanggwp <backupkamera3300@gmail.com>
2023-08-20 14:09:04 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
 </head>
 <body>
     <h2>Hello world</h2>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
+
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis fuga aliquid vitae quasi eum atque earum facere! Soluta eaque, corrupti minima doloremque voluptate repudiandae. Magnam earum corrupti vel iste ut.
     Officia sint ut omnis quisquam eaque fugit. Possimus quidem omnis aperiam perferendis error, quae placeat. Dolores iusto minus doloremque quia unde quisquam, quae libero tempora nam quaerat? Enim, tempore totam?
     Perferendis placeat mollitia aperiam. Id quidem vitae possimus nisi totam tempora magni quis voluptatum ad? Vero repudiandae, iste cupiditate consectetur earum dolorem amet. Fuga at suscipit facilis, adipisci in est?</p>
